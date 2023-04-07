7c8ec215a41fb5b1ad3b3bfa52f45f3205778996
John Wooley <john.m.wooley@gmail.com>
2023-04-06 23:28:38 -0500

    Updated Project List

--- src/components/project_list.js
+++ src/components/project_list.js
@@ -4,59 +4,72 @@ import PD_Image from '../assets/static/PD_Preview.png'
 import WA_Image from '../assets/static/Weather_App_Preview.png'
 import AG_Image from '../assets/static/Advice_Generator_Preview.jpg'
 import Blog_Img from '../assets/static/Logo_concept_2.jpg'
+import WA2_Image from "../assets/static/Weather2.0.jpg";
 // import { Javascript } from '@mui/icons-material'
 
 export const project_list = [
-    {
-        title: 'QR-Code-Comp',
-        description: 'A QR code app provided by Frontend Mentor',
-        image: QR_Code,
-        tags: ['HTML', 'CSS'],
-        source: 'https://github.com/jaywooski/QR-Code-Comp',
-        liveVersion: 'https://jaywooski.github.io/QR-Code-Comp/',
-        id: 1
-    },
-    
-    {
-        title: 'Programmer Dashboard',
-        description: 'A dashboard utilizing third party APIs which keeps track of recent articles appealing to programmers like ourselves.',
-        image: PD_Image,
-        tags: ['JavaScript','HTML', 'CSS', 'API'],
-        source: 'https://github.com/jaywooski/Programmer-Dashboard',
-        liveVersion: 'https://joelkovalcson.github.io/Programmer-Dashboard/',
-        id: 2
-    },
+	{
+		title: "QR-Code-Comp",
+		description: "A QR code app provided by Frontend Mentor",
+		image: QR_Code,
+		tags: ["HTML", "CSS"],
+		source: "https://github.com/jaywooski/QR-Code-Comp",
+		liveVersion: "https://jaywooski.github.io/QR-Code-Comp/",
+		id: 1,
+	},
 
-    {
-        title: 'Wooley Weather',
-        description: 'A weather checking app dashboard using OpenWeatherMap API functionality',
-        image: WA_Image,
-        tags: ['JavaScript', 'HTML', 'CSS', 'API'],
-        source: 'https://github.com/jaywooski/Wooley-Weather-Dashboard-Chal-6',
-        liveVersion: 'https://jaywooski.github.io/Wooley-Weather-Dashboard-Chal-6/',
-        id: 3
-    },
+	{
+		title: "Programmer Dashboard",
+		description:
+			"A dashboard utilizing third party APIs which keeps track of recent articles appealing to programmers like ourselves.",
+		image: PD_Image,
+		tags: ["JavaScript", "HTML", "CSS", "API"],
+		source: "https://github.com/jaywooski/Programmer-Dashboard",
+		liveVersion: "https://joelkovalcson.github.io/Programmer-Dashboard/",
+		id: 2,
+	},
 
-    {
-        title: 'Advice Generator',
-        description: `Here's an app that generates some helpful advice to live by.
+	{
+		title: "Wooley Weather",
+		description:
+			"A weather checking app dashboard using OpenWeatherMap API functionality",
+		image: WA_Image,
+		tags: ["JavaScript", "HTML", "CSS", "API"],
+		source: "https://github.com/jaywooski/Wooley-Weather-Dashboard-Chal-6",
+		liveVersion:
+			"https://jaywooski.github.io/Wooley-Weather-Dashboard-Chal-6/",
+		id: 3,
+	},
+
+	{
+		title: "Advice Generator",
+		description: `Here's an app that generates some helpful advice to live by.
         Powered by the Advice Slip API Provider`,
-        image: AG_Image,
-        tags: ['JavaScript', 'HTML', 'CSS', 'API'],
-        source: 'https://github.com/jaywooski/advice-generator-app-main',
-        liveVersion: 'https://jaywooski.github.io/advice-generator-app-main/',
-        id: 4
-    },
+		image: AG_Image,
+		tags: ["JavaScript", "HTML", "CSS", "API"],
+		source: "https://github.com/jaywooski/advice-generator-app-main",
+		liveVersion: "https://jaywooski.github.io/advice-generator-app-main/",
+		id: 4,
+	},
 
-    {
-        title: 'Personal Blog',
-        description: `I've created my own personal blog page, powered by Wordpress.com.
+	{
+		title: "Personal Blog",
+		description: `I've created my own personal blog page, powered by Wordpress.com.
         Check it out!`,
-        image: Blog_Img,
-        tags: ['Wordpress'],
-        source: null,
-        liveVersion: 'https://jaywooski.com',
-        id: 5
-    }
+		image: Blog_Img,
+		tags: ["Wordpress"],
+		source: null,
+		liveVersion: "https://jaywooski.com",
+		id: 5,
+	},
 
-]
+	{
+		title: "Wooley Weather 2.0",
+		description: `My previous weather app has been revamped using react and chakra UI`,
+		image: WA2_Image,
+		tags: ["Chakra UI", "React", "CSS", "API"],
+		source: "https://github.com/jaywooski/weather-app-revamped",
+		liveVersion: "https://stately-manatee-33c931.netlify.app/",
+		id: 6,
+	},
+];
